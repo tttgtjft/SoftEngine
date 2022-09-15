d940277ee74d257126dbd01e43afae260c4e0fb2
Nikita <1onecov@gmail.com>
2022-09-15 21:46:21 +0300

    Added GLFW

---
+++ SoftEngineCore/includes/SoftEngineCore/Utils/test.hpp
@@ -0,0 +1,5 @@
+namespace SoftEngine {
+
+	int checkGLFW();
+
+}
---
+++ SoftEngineEditor/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <SoftEngineCore/Utils/test.hpp>
+
+int main() {
+	std::cout << "Hello from Soft Engine Editor" << std::endl;
+
+	SoftEngine::checkGLFW();
+
+	return 0;
+}
