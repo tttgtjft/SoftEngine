9ed04c274b15975f2f8e1584dde5547dfd0fd27d
Nikita <1onecov@gmail.com>
2022-09-21 21:19:04 +0300

    Added spdlog

--- build/SoftEngineCore/cmake_install.cmake
+++ build/SoftEngineCore/cmake_install.cmake
@@ -37,3 +37,8 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/ASUS TUF GAMING/Desktop/SoftEngine/build/SoftEngineCore/glfw/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/ASUS TUF GAMING/Desktop/SoftEngine/build/SoftEngineCore/spdlog/cmake_install.cmake")
+endif()
+
