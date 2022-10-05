d064f47ee3c6e83c4f9b05327a4a4180a50dadb9
Nikita <1onecov@gmail.com>
2022-10-06 01:20:43 +0300

    Added VertexBuffer class

--- SoftEngineCore/includes/SoftEngineCore/Application.hpp
+++ SoftEngineCore/includes/SoftEngineCore/Application.hpp
@@ -20,6 +20,7 @@ namespace SoftEngine {
 		virtual int start(unsigned int window_weight, unsigned int window_height, const char* title);
 
 		virtual void on_update() {}
+
 	private:
 		std::unique_ptr<class Window> m_pWindow;
 
